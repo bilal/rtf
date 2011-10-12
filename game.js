a8d3e2143108d9da7d4986a7e1af64183b60584c
Ahmed Wasfy <awasfy@ea.com>
2011-10-12 14:18:18 -0400

    Fifa 0.1

---
+++ game.js
@@ -0,0 +1,304 @@
+window.onload = function() {
+	//start crafty
+	Crafty.init(400, 320);
+	Crafty.canvas();
+	
+	//turn the sprite map into usable components
+	Crafty.sprite(16, "sprite2.png", {
+		grass1: [0,0],
+		grass2: [1,0],
+		grass3: [2,0],
+		grass4: [3,0],
+		bush1: [0,2],
+		bush2: [1,2],
+		player1: [0,3],
+		player2: [0,1],
+		goal: [0,4],
+		ball: [0, 5]
+	});
+	
+	//method to randomy generate the map
+	function generateWorld() {
+		//generate the grass along the x-axis
+		for(var i = 0; i < 25; i++) {
+			//generate the grass along the y-axis
+			for(var j = 0; j < 20; j++) {
+				grassType = Crafty.randRange(1, 4);
+				Crafty.e("2D, Canvas, grass"+grassType)
+					.attr({x: i * 16, y: j * 16});
+				
+			}
+		}
+		
+		//create the bushes along the x-axis which will form the boundaries
+		for(var i = 0; i < 25; i++) {
+			Crafty.e("2D, Canvas, wall_top, bush"+Crafty.randRange(1,2))
+				.attr({x: i * 16, y: 0, z: 2});
+			Crafty.e("2D, DOM, wall_bottom, bush"+Crafty.randRange(1,2))
+				.attr({x: i * 16, y: 304, z: 2});
+		}
+		
+
+		//create the bushes along the y-axis
+		//we need to start one more and one less to not overlap the previous bushes
+		for(var i = 1; i < 19; i++) {
+		
+			//skip goals
+			if((i*16) >= 140 && (i*16) <= 200) continue;
+			
+			Crafty.e("2D, DOM, wall_left, bush"+Crafty.randRange(1,2))
+				.attr({x: 0, y: i * 16, z: 2});
+			Crafty.e("2D, Canvas, wall_right, bush"+Crafty.randRange(1,2))
+				.attr({x: 384, y: i * 16, z: 2});
+		}
+		
+		//create the goals
+		for(var i = 0; i<5; i++) {
+		
+		Crafty.e("2D, Canvas, goal_left, goal")
+			.attr({x: 0, y: (145 + (i*10)), z: 2});
+		
+		Crafty.e("2D, Canvas, goal_right, goal")
+			.attr({x: 384, y: (145 + (i*10)), z: 2});	
+		}
+		
+
+		
+	}
+	
+	//the loading screen that will display while our assets load
+	Crafty.scene("loading", function() {
+		//load takes an array of assets and a callback when complete
+		Crafty.load(["sprite2.png"], function() {
+			Crafty.scene("main"); //when everything is loaded, run the main scene
+		});
+		
+		//black background with some loading text
+		Crafty.background("#000");
+		Crafty.e("2D, DOM, Text").attr({w: 100, h: 20, x: 150, y: 120})
+			.text("Loading")
+			.css({"text-align": "center"});
+	});
+	
+	//automatically play the loading scene
+	Crafty.scene("loading");
+	
+	Crafty.scene("main", function() {
+		generateWorld();
+		
+		Crafty.c('CustomControls1', {
+			__move: {left: false, right: false, up: false, down: false},	
+			_speed: 3,
+			
+			CustomControls: function(speed) {
+				if(speed) this._speed = speed;
+				var move = this.__move;
+				
+				this.bind('enterframe', function() {
+					//move the player in a direction depending on the booleans
+					//only move the player in one direction at a time (up/down/left/right)
+					if(this.isDown("RIGHT_ARROW")){
+						if(this.isDown("SHIFT")) this.x += this._speed*2;
+						else this.x += this._speed;} 
+					else if(this.isDown("LEFT_ARROW")){ 
+						if(this.isDown("SHIFT")) this.x -= this._speed*2;
+						else this.x -= this._speed; }
+					else if(this.isDown("UP_ARROW")){
+						if(this.isDown("SHIFT")) this.y -= this._speed*2;
+						else this.y -= this._speed; }	
+					else if(this.isDown("DOWN_ARROW")){
+						if(this.isDown("SHIFT")) this.y += this._speed*2;
+						else this.y += this._speed; }
+					
+				});
+				
+				return this;
+			}
+		});
+		
+				Crafty.c('CustomControls2', {
+			__move: {left: false, right: false, up: false, down: false},	
+			_speed: 3,
+			
+			CustomControls: function(speed) {
+				if(speed) this._speed = speed;
+				var move = this.__move;
+				
+				this.bind('enterframe', function() {
+					//move the player in a direction depending on the booleans
+					//only move the player in one direction at a time (up/down/left/right)
+			
+					
+					 if(this.isDown("D")) {
+					 	if(this.isDown("G")) this.x += this._speed*2;
+						else this.x += this._speed; }
+					else if(this.isDown("A")){
+						if(this.isDown("G")) this.x -= this._speed*2;
+						else this.x -= this._speed; }
+					else if(this.isDown("W")) {
+						if(this.isDown("G")) this.y -= this._speed*2;
+						else this.y -= this._speed;} 	
+					else if(this.isDown("S")) {
+						if(this.isDown("G")) this.y += this._speed*2;
+						else this.y += this._speed; }
+				});
+				
+				return this;
+			}
+		});
+		
+		//ball
+		ball = Crafty.e("2D, Canvas, ball, Controls3, CustomControls3, Animate, Collision")
+			.attr({x: 182, y: 144, z: 1})
+			.animate("ball_1", 1, 5, 3)
+		.collision()
+			.onHit("wall_left", function() {
+				this.x += this._speed;
+				this.stop();
+			}).onHit("goal_left", function() {
+				this.x += this._speed*2;
+				this.stop();
+			}).onHit("goal_right", function() {
+				this.x -= this._speed*2;
+				this.stop();
+			}).onHit("wall_right", function() {
+				this.x -= this._speed;
+				this.stop();
+			}).onHit("wall_bottom", function() {
+				this.y -= this._speed;
+				this.stop();
+			}).onHit("wall_top", function() {
+				this.y += this._speed;
+				this.stop();
+			})	
+		
+		
+		var player1HasBall = false;
+		//create our player entity with some premade components
+		player1 = Crafty.e("2D, Canvas, player1, Controls, CustomControls1, Animate, Collision")
+			.attr({x: 160, y: 144, z: 1})
+			.CustomControls(1)
+			.animate("walk_left", 6, 3, 8)
+			.animate("walk_right", 9, 3, 11)
+			.animate("walk_up", 3, 3, 5)
+			.animate("walk_down", 0, 3, 2)
+			.bind("enterframe", function(e) {
+				if(this.isDown("LEFT_ARROW")) {
+					if(!this.isPlaying("walk_left"))
+							this.stop().animate("walk_left", 10);
+				} else if(this.isDown("RIGHT_ARROW")) {
+					if(!this.isPlaying("walk_right"))
+						this.stop().animate("walk_right", 10);
+				} else if(this.isDown("UP_ARROW")) {
+					if(!this.isPlaying("walk_up"))
+						this.stop().animate("walk_up", 10);
+				} else if(this.isDown("DOWN_ARROW")) {
+					if(!this.isPlaying("walk_down"))
+						this.stop().animate("walk_down", 10);
+				}
+				
+			}).bind("keyup", function(e) {
+				this.stop();
+			})
+			.collision()
+			.onHit("wall_left", function() {
+				this.x += this._speed;
+				this.stop();
+			}).onHit("goal_left", function() {
+				this.x += this._speed*2;
+				this.stop();
+			}).onHit("goal_right", function() {
+				this.x -= this._speed*2;
+				this.stop();
+			}).onHit("wall_right", function() {
+				this.x -= this._speed;
+				this.stop();
+			}).onHit("wall_bottom", function() {
+				this.y -= this._speed;
+				this.stop();
+			}).onHit("wall_top", function() {
+				this.y += this._speed;
+				this.stop();
+			}).onHit("ball", function() {
+				ball.animate("ball_1", 10);
+				if(this.isPlaying("walk_left")){
+					ball.x -= this._speed;
+				}
+				else if(this.isPlaying("walk_right")){
+					ball.x += this._speed;
+				}
+				else if(this.isPlaying("walk_up")){
+					ball.y -= this._speed;
+				}
+				else if(this.isPlaying("walk_down")){
+					ball.y += this._speed;
+				}
+				this.stop();
+			});
+
+			
+			
+					//create our player entity with some premade components
+		player2 = Crafty.e("2D, Canvas, player2, Controls, CustomControls2, Animate, Collision")
+			.attr({x: 160, y: 10, z: 1})
+			.CustomControls(1)
+			.animate("walk_left", 6, 1, 8)
+			.animate("walk_right", 9, 1, 11)
+			.animate("walk_up", 3, 1, 5)
+			.animate("walk_down", 0, 1, 2)
+			.bind("enterframe", function(e) {
+				if(this.isDown("A")) {
+					if(!this.isPlaying("walk_left"))
+						this.stop().animate("walk_left", 10);
+				} else if(this.isDown("D")) {
+					if(!this.isPlaying("walk_right"))
+						this.stop().animate("walk_right", 10);
+				} else if(this.isDown("W")) {
+					if(!this.isPlaying("walk_up"))
+						this.stop().animate("walk_up", 10);
+				} else if(this.isDown("S")) {
+					if(!this.isPlaying("walk_down"))
+						this.stop().animate("walk_down", 10);
+				}
+			}).bind("keyup", function(e) {
+				this.stop();
+			})
+			.collision()
+			.onHit("wall_left", function() {
+				this.x += this._speed;
+				this.stop();
+			}).onHit("goal_left", function() {
+				this.x += this._speed*2;
+				this.stop();
+			}).onHit("goal_right", function() {
+				this.x -= this._speed*2;
+				this.stop();
+			}).onHit("wall_right", function() {
+				this.x -= this._speed;
+				this.stop();
+			}).onHit("wall_bottom", function() {
+				this.y -= this._speed;
+				this.stop();
+			}).onHit("wall_top", function() {
+				this.y += this._speed;
+				this.stop();
+			}).onHit("ball", function() {
+				ball.animate("ball_1", 10);
+				if(this.isPlaying("walk_left")){
+					ball.x -= this._speed;
+				}
+				else if(this.isPlaying("walk_right")){
+					ball.x += this._speed;
+				}
+				else if(this.isPlaying("walk_up")){
+					ball.y -= this._speed;
+				}
+				else if(this.isPlaying("walk_down")){
+					ball.y += this._speed;
+				}
+				this.stop();
+			});;
+			
+			
+	});
+};
