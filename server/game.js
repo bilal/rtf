903d46315130c44faa205dae36be40d8f068c30f
Ahmed Wasfy <awasfy@ea.com>
2011-10-14 11:40:13 -0400

    ball collision update

--- server/game.js
+++ server/game.js
@@ -245,7 +245,9 @@ window.onload = function() {
 			}).onHit("wall_top", function() {
 				this.y += this._speed;
 				this.stop();
-			}).onHit("ball", function() {
+			})
+			.collision(new Crafty.polygon([ball.x,ball.y], [16, 16]))
+			.onHit("ball", function() {
 				player1CanShoot = true;
 				if(this.isPlaying("walk_left")){
 					webSocket.emit('game_move',{player:"1", direction:"left", move:"BALL", speed:this._speed});
@@ -294,7 +296,9 @@ window.onload = function() {
 			}).onHit("wall_top", function() {
 				this.y += this._speed;
 				this.stop();
-			}).onHit("ball", function() {
+			})
+			.collision(new Crafty.polygon([ball.x,ball.y], [16, 16]))
+			.onHit("ball", function() {
 				player2CanShoot = true;
 				if(this.isPlaying("walk_left")){
 					webSocket.emit('game_move',{player:"2", direction:"left", move:"BALL", speed:this._speed});
