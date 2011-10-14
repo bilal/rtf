5430288b6e2cfa4e68f6043843582118af4d030b
Bilal Sheikh <msheikh@ea.com>
2011-10-13 20:23:12 -0400

    awesome graphics

--- server/game.js
+++ server/game.js
@@ -1,15 +1,14 @@
-
 //Screen Size variables
-var X_SIZE = 800;
-var Y_SIZE = 640;
+var X_SIZE = 780;
+var Y_SIZE = 600;
 
-var SPRITE_SIZE = 16;
+var SPRITE_SIZE = 32;
 var X_BLOCKS = X_SIZE/SPRITE_SIZE;
 var Y_BLOCKS = Y_SIZE/SPRITE_SIZE;
 
 //Goal related variables
 var GOAL_PATCH_SIZE = 10;
-var GOAL_SIZE = 60;
+var GOAL_SIZE = 128;
 var GOAL_EMPTY_SPACE = 10;
 var GOAL_NUM_PATCHES = (GOAL_SIZE - GOAL_EMPTY_SPACE)/GOAL_PATCH_SIZE;
 var GOAL_START = (Y_SIZE - GOAL_SIZE)/2;
@@ -57,37 +56,48 @@ window.onload = function() {
 	Crafty.canvas();
 	
 	//turn the sprite map into usable components
-	Crafty.sprite(SPRITE_SIZE, "sprite2.png", {
+	Crafty.sprite(SPRITE_SIZE, "sprite4.png", {
 		grass1: [0,0],
 		grass2: [1,0],
 		grass3: [2,0],
 		grass4: [3,0],
-		bush1: [0,2],
-		bush2: [1,2],
-		player1: [0,3],
+		player1: [0,2],
 		player2: [0,1],
-		goal: [0,4],
-		ball: [0, 5]
+		goal: [0,3],
+		ball: [0, 4],
+		line: [1,3],
+		hline: [2,3]
 	});
 	
 	//method to randomy generate the map
 	function generateWorld() {
+	var temp = 1;
+	var temp2=1;
 		//generate the grass along the x-axis
 		for(var i = 0; i < X_BLOCKS; i++) {
+		temp++;
 			//generate the grass along the y-axis
 			for(var j = 0; j < Y_BLOCKS; j++) {
-				grassType = Crafty.randRange(1, 4);
+				
+				grassType = Crafty.randRange(1, 2);
+				grassType2 = Crafty.randRange(3,4);
+				if (temp < 6)
 				Crafty.e("2D, Canvas, grass"+grassType)
+				.attr({x: i * SPRITE_SIZE, y: j * SPRITE_SIZE});
+				else Crafty.e("2D, Canvas, grass"+grassType2)
 					.attr({x: i * SPRITE_SIZE, y: j * SPRITE_SIZE});
+					
+					
+					if (temp > 10) temp =1;
 				
 			}
 		}
 		
 		//create the bushes along the x-axis which will form the boundaries
-		for(var i = 0; i < X_BLOCKS; i++) {
-			Crafty.e("2D, Canvas, wall_top, bush"+Crafty.randRange(1,2))
+		for(var i = 1; i < X_BLOCKS - 1; i++) {
+			Crafty.e("2D, Canvas, wall_top, hline")
 				.attr({x: i * SPRITE_SIZE, y: 0, z: 2});
-			Crafty.e("2D, DOM, wall_bottom, bush"+Crafty.randRange(1,2))
+			Crafty.e("2D, DOM, wall_bottom, hline")
 				.attr({x: i * SPRITE_SIZE, y: PLAY_AREA_Y_MAX, z: 2});
 		}
 		
@@ -99,9 +109,9 @@ window.onload = function() {
 			//skip goals
 			if((i*SPRITE_SIZE) >= GOAL_START && (i*SPRITE_SIZE) <= GOAL_END) continue;
 			
-			Crafty.e("2D, DOM, wall_left, bush"+Crafty.randRange(1,2))
+			Crafty.e("2D, DOM, wall_left, line")
 				.attr({x: 0, y: i * SPRITE_SIZE, z: 2});
-			Crafty.e("2D, Canvas, wall_right, bush"+Crafty.randRange(1,2))
+			Crafty.e("2D, Canvas, wall_right, line")
 				.attr({x: PLAY_AREA_X_MAX, y: i * SPRITE_SIZE, z: 2});
 		}
 		
@@ -122,7 +132,7 @@ window.onload = function() {
 	//the loading screen that will display while our assets load
 	Crafty.scene("loading", function() {
 		//load takes an array of assets and a callback when complete
-		Crafty.load(["sprite2.png"], function() {
+		Crafty.load(["sprite3.png"], function() {
 			Crafty.scene("main"); //when everything is loaded, run the main scene
 		});
 		
@@ -177,7 +187,7 @@ window.onload = function() {
 		//ball
 		ball = Crafty.e("2D, Canvas, ball, Controls3, CustomControls3, Animate, Collision")
 			.attr({x: INIT_BALL_POS_X, y: INIT_BALL_POS_Y, z: 1})
-			.animate("ball_1", 1, 5, 3)
+			.animate("ball_1", 1, 4, 3)
 		.collision()
 			.onHit("wall_left", function() {
 				logger("out of bounds");
@@ -205,10 +215,10 @@ window.onload = function() {
 		player1 = Crafty.e("2D, Canvas, player1, Controls, CustomControls1, Animate, Collision")
 			.attr({x: INIT_P1_POS_X, y: INIT_P1_POS_Y, z: 1})
 			.CustomControls(1)
-			.animate("walk_left", 6, 3, 8)
-			.animate("walk_right", 9, 3, 11)
-			.animate("walk_up", 3, 3, 5)
-			.animate("walk_down", 0, 3, 2)
+			.animate("walk_left", 6, 2, 8)
+			.animate("walk_right", 9, 2, 11)
+			.animate("walk_up", 3, 2, 5)
+			.animate("walk_down", 0, 2, 2)
 			.bind("keyup", function(e) {
 				this.stop();
 			})
